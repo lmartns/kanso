6e35b473867664143aa2c77817784232bb16e236
lmartns <leonardomartins578@gmail.com>
2025-06-24 21:26:51 -0300

    ✨ feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-background text-foreground">
+      <h1 className="text-3xl font-bold">Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
