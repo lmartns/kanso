d119ea03d1867e4d8812e8987c557c74beab2d8b
lmartns <leonardomartins578@gmail.com>
2025-07-06 20:13:25 -0300

    fix

--- src/components/WriterEditor.tsx
+++ src/components/WriterEditor.tsx
@@ -1,45 +1,46 @@
-import StarterKit from '@tiptap/starter-kit'
-import { Color } from '@tiptap/extension-color'
-import TextStyle from '@tiptap/extension-text-style'
-import ListItem from '@tiptap/extension-list-item'
-import { EditorProvider, useCurrentEditor } from '@tiptap/react'
+import '../index.css'
 
-const WriteEditor = () => {
+import { Color } from '@tiptap/extension-color'
+import ListItem from '@tiptap/extension-list-item'
+import TextStyle from '@tiptap/extension-text-style'
+import { EditorProvider, useCurrentEditor } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
+
+const MenuBar = () => {
   const { editor } = useCurrentEditor()
 
   if (!editor) {
     return null
   }
 
-  return (
-    <div className="control-group">
-    </div>
-  )
+  return <div className="control-group"></div>
 }
 
 const extensions = [
   Color.configure({ types: [TextStyle.name, ListItem.name] }),
-  TextStyle,
+  TextStyle.configure({ types: [ListItem.name] }),
   StarterKit.configure({
     bulletList: {
       keepMarks: true,
-      keepAttributes: false, 
+      keepAttributes: false, // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
     },
-    orderedList: { 
+    orderedList: {
       keepMarks: true,
-      keepAttributes: false,
+      keepAttributes: false, // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
     },
-    }),
-  ListItem,
+  }),
 ]
 
-const WriterEditor = () => {
+const content = `
+<h1>Kanso</h1>
+`
+
+export default () => {
   return (
     <EditorProvider
-      slotBefore={<WriteEditor />}
+      slotBefore={<MenuBar />}
       extensions={extensions}
+      content={content}
     ></EditorProvider>
   )
 }
-
-export default WriterEditor
