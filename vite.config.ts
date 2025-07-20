6848c9d175bc9eb3a91e59f05be8a2c7d80c7f47
lmartns <leonardomartins578@gmail.com>
2025-07-20 20:12:41 -0300

    hotfix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react' 
 
 export default defineConfig({
-  base: '/kanso/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
