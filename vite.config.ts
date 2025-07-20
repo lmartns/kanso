68e5e17acdbfc30e6dbfd92abdf3c6185419391c
lmartns <leonardomartins578@gmail.com>
2025-07-20 09:54:31 -0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import path from "path"
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react' 
 
 export default defineConfig({
-  plugins: [tailwindcss()],
+  base: '/kanso/',
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
